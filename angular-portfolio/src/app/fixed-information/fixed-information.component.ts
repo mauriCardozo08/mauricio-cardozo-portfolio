51b615886af21013c10cb38b9ec7762fe6df1e3d
mcardozo <mcardozo@urudatasoftware.com>
2025-06-07 23:12:12 -0300

    wip: add font styles and other details

--- angular-portfolio/src/app/fixed-information/fixed-information.component.ts
+++ angular-portfolio/src/app/fixed-information/fixed-information.component.ts
@@ -1,11 +1,26 @@
 import { Component } from '@angular/core';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import {NgxTypedJsModule} from 'ngx-typed-js';
+import { AppConsts } from '../../shared/app.consts';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-fixed-information',
-  imports: [],
+  imports: [FontAwesomeModule,NgxTypedJsModule,CommonModule],
   templateUrl: './fixed-information.component.html',
   styleUrl: './fixed-information.component.scss'
 })
 export class FixedInformationComponent {
+  technologies: string[] = AppConsts.Technologies;  
+  isRepeat: boolean = false;
 
+  onStringTyped(index: number): void {
+    const currentWord = this.technologies[index];
+    this.isRepeat = currentWord === 'Repeat';
+  }
+
+  obBeforeStringTyped(index: number): void{
+    const currentWord = this.technologies[index];
+    this.isRepeat = !(currentWord != 'Repeat');
+  }
 }
---
+++ angular-portfolio/src/shared/app.consts.ts
@@ -0,0 +1,4 @@
+export class AppConsts {
+    public static readonly Technologies: string[] = ['Think', 'Design', 'Code', 'Test', 'Deploy', 'Refactor', 'Repeat'];
+    ;
+}
